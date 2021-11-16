bbfeaf2115fbae1f312f245fd8f8952767d8dedc
Jianna Liu <jianna@mit.edu>
2021-11-16 16:22:18 -0500

    add initial leaflet

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router/index';
 import store from './store/index';
+import 'leaflet/dist/leaflet.css';
 
 
 Vue.config.productionTip = false;
