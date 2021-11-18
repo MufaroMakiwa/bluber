576e89e9c036e9cb44ab37977ad29d2f290929fd
Mufaro Makiwa <mmakiwa@mit.edu>
2021-11-18 06:22:30 -0500

    Adds stubs for routes

---
+++ routes/mark.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const userModel = require("../models/user");
+const commentModel = require("../models/comment");
+const markModel = require("../models/mark");
+const ratingModel = require("../models/rating");
+const savedModel = require("../models/saved");
+const replyModel = require("../models/reply");
+
+
+
+
+module.exports = router;
